34863b4b1f298ca18c6b7c30655b3e06aacb3a43
rayanneo737 <132960056+rayanneo737@users.noreply.github.com>
2024-10-10 21:28:06 -0300

    Delete Big data/Aula_2 directory

--- Big data/Aula_2/praticando_03.py
+++
@@ -1,7 +0,0 @@
-prestacao = float(input("Informe o valor da prestação: "))
-taxa = float(input("Informe a taxa: "))
-tempo = int(input("Quantos meses tem de atraso: "))
-
-valorFinal = prestacao + (prestacao*(taxa/100)*tempo)
-
-print (valorFinal)
