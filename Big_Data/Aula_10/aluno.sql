f892fcc9d927380fcfabc4af56efbc051487637c
rayanneo737 <132960056+rayanneo737@users.noreply.github.com>
2024-10-15 18:19:27 -0300

    Add files via upload

---
+++ Big_Data/Aula_10/aluno.sql
@@ -0,0 +1,75 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Tempo de geração: 11-Out-2024 às 22:22
+-- Versão do servidor: 5.7.36
+-- versão do PHP: 8.1.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `escolar`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `aluno`
+--
+
+CREATE TABLE `aluno` (
+  `matricula` smallint(6) NOT NULL,
+  `nome` varchar(60) NOT NULL,
+  `idade` smallint(6) NOT NULL,
+  `sexo` varchar(15) NOT NULL,
+  `numero` smallint(6) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `aluno`
+--
+
+INSERT INTO `aluno` (`matricula`, `nome`, `idade`, `sexo`, `numero`) VALUES
+(11002, 'Alessandro Vieira', 50, 'M', 1101),
+(11003, 'Maria Penha', 80, 'F', 1101),
+(11004, 'Eduarda Silva', 20, 'F', 1301),
+(11005, 'Dirceu Amorim', 70, 'M', 1201),
+(11006, 'Pedro José', 15, 'M', 1201),
+(11007, 'Suelen Costa', 17, 'F', 1001),
+(11008, 'Erika Coimbra', 16, 'F', 1001);
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices para tabela `aluno`
+--
+ALTER TABLE `aluno`
+  ADD PRIMARY KEY (`matricula`),
+  ADD KEY `numero` (`numero`);
+
+--
+-- Restrições para despejos de tabelas
+--
+
+--
+-- Limitadores para a tabela `aluno`
+--
+ALTER TABLE `aluno`
+  ADD CONSTRAINT `aluno_ibfk_1` FOREIGN KEY (`numero`) REFERENCES `turma` (`numero`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
